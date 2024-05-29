72f1e1b992141d72cc0a77a6686eed3c1d372e51
jun-young1993 <juny3738@gmail.com>
2024-05-29 19:07:32 +0900

    v1.0.3

--- src/component/svgContainer/SvgContainer.tsx
+++ src/component/svgContainer/SvgContainer.tsx
@@ -2,6 +2,28 @@ import { SVGProps } from 'react';
 export interface SvgType extends SVGProps<SVGSVGElement>{
 	data: string
 	ariaHidden?: boolean
+	strokeColor?: string;
+	strokeWidth?: string;
+	strokeWidth2?: string;
+	strokeWidth3?: string;
+	strokeFill?: string;
+	fillColor?: string;
+	fillColor2?: string;
+	fillColor3?: string;
+	fillColor4?: string;
+	fillColor5?: string;
+	fillColor6?: string;
+	fillColor7?: string;
+	imageWidth?: string;
+	imageHeight?: string;
+	width?: string;
+	height?: string;
+	rotateCenter?: number;
+	className?: string;
+	className2?: string;
+	className3?: string;
+	className4?: string;
+	className5?: string;
 }
 
 const SvgContainer = ({
