fa0338acc450588e195444fc7d40061073708674
junyoungkim <juny3738@gmail.com>
2024-06-06 16:35:13 +0900

    v1.0.6

--- src/stories/sun.stories.tsx
+++ src/stories/sun.stories.tsx
@@ -11,5 +11,8 @@ const Template: StoryFn<typeof SunIcon> = (args) => <SunIcon {...args} />
 
 export const Default = Template.bind({});
 Default.args = {
+  viewBox: "0 0 128 128",
+  width: "100",
+  height: "100"
   // 기본값을 설정할 수 있습니다.
 };
